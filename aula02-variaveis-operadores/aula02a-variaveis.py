27fdd9e2c8b15aa18f7d9f6504542739a2a814d8
matheusacsoares <macsoares1275@gmail.com>
2026-03-16 20:45:51 -0300

    Aula 2 - variaveis

---
+++ aula02-variaveis-operadores/aula02a-variaveis.py
@@ -0,0 +1,33 @@
+print("Olá, mundo")
+
+print(7+4)
+
+print('7'+'4') # CONCATENANDO STRINGS
+
+'''
+Comentários
+de
+Múltiplas
+Linhas
+'''
+
+# VARIÁVEIS
+
+nome = 'Matheus'
+idade = 18
+peso = 70
+
+print(nome, idade, peso)
+
+print(f"Olá {nome}!")
+
+#INPUTS -- SIMULAÇÃO DE UM FORMS NO CMD
+
+nome = input("Digite seu nome:")
+idade = input("Digite sua idade: ")
+peso = input("Digite seu peso: ")
+
+print(nome, idade, peso)
+
+
+
---
+++ aula02-variaveis-operadores/desafios.py
@@ -0,0 +1,29 @@
+# DESAFIO 1
+
+nome = input("Digite seu nome: ")
+
+print(f"Seja bem-vindo {nome}")
+
+print("=======================")
+
+# DESAFIO 2
+
+print("Digite as informações da sua data de nascimento abaixo")
+dia = input("Dia: ")
+mes = input("Mês: ")
+ano = input("Ano: ")
+
+print(f"Você nasceu em {dia}/{mes}/{ano}")
+
+print("=======================")
+
+# DESAFIO 3
+
+print("Somar 2 valores")
+
+n1 = float(input("Dígite o primeiro valor: "))
+n2 = float(input("Dígite o segundo valor: "))
+
+print(f"A soma de {n1} + {n2} é {n1+n2}")
+
+print("=======================")
