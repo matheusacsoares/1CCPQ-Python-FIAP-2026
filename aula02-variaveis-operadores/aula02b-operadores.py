5c69bbfeb973f843c250bf76fdcb86e3a4bee8dd
matheusacsoares <macsoares1275@gmail.com>
2026-03-30 19:58:45 -0300

    aula 2 - operadores

---
+++ aula02-variaveis-operadores/aula02b-operadores.py
@@ -0,0 +1,51 @@
+
+
+n1 = 5
+n2 = 2
+
+print(type(n1), type(n2))
+
+resultadoOperacao = n1 / n2
+
+print(resultadoOperacao, type(resultadoOperacao))
+
+#OPERADORES DE ATRIBUIÇÃO
+
+num = 15
+
+print() #pular linha
+print(num)
+
+num = num + 2 #acumulando 2 em num
+print(num)
+#ou
+num += 2
+print(num)
+
+# OPERADORES RELACIONAIS
+
+print(6 == 2) # retorna resultado booleano baseado na relação estabelecida
+
+idade = 20
+print(idade == 10)
+print(idade >= 21)
+
+logado = True
+print(logado, type(logado))
+
+maiorIdade = idade >= 18 # atribui à variável um valor booleano com base na relação estabelecida
+print(maiorIdade, type(maiorIdade))
+
+nome1 = "Marcos"
+nome2 = "marcos"
+
+print(nome1 == nome2) # vai retornar false pois a letra inicial é maiuscula no 1° e minuscula no 2°
+
+print(nome1.upper() == nome2.upper()) # vai tornar a string inteira em uppercase e então, retornar True
+
+#
+
+
+
+
+
